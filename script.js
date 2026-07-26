73d02f15d99b5d879ce48863cfc834170ffe2e22
it'z Badhon <144930401+1badhon@users.noreply.github.com>
2026-07-27 02:10:18 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#38bdf8"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.6
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#38bdf8",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      }
+    },
+    modes: {
+      grab: {
+        distance: 150,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      push: {
+        particles_nb: 4
+      }
+    }
+  },
+  retina_detect: true
+});
