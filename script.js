833ccfc4c1f9cc9eb5d985882d964d48890855d8
it'z Badhon <144930401+1badhon@users.noreply.github.com>
2026-07-27 02:18:50 +0600

    Add typing effect for text display

--- script.js
+++ script.js
@@ -57,3 +57,15 @@ particlesJS("particles-js", {
   },
   retina_detect: true
 });
+const text = "Aspiring Network Engineer";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 100);
+  }
+}
+
+typing();
